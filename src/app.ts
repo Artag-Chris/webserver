3f1cad0baac7e8c575b13cdd47ffd83ad579b63f
Artag/Chris <scristxyz@gmail.com>
2024-08-25 19:38:31 -0500

    hello

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import { envs } from './config';
+
+
+(()=>{
+    main()
+})();
+
+
+function main(){
+
+    const app=express();
+
+
+    app.post(`/api/github`,(req,res)=>{
+        res.json(`Github Endpoint`)
+    })
+
+
+    app.listen(envs.PORT,()=>{
+        console.log(`http://localhost:${envs.PORT}`);
+    });
+
+
+
+}
